1f0c93be1af168547c48b9fc4826f1b979eabc7a
monirshikdar26 <monirshikdar26@gmail.com>
2021-08-16 00:57:34 +0600

    third commit

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,16 @@
+
+
+
+  <footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+		  <li class="nav-item">
+			<a class="nav-link" href="javascript:void(0)">SEU</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="#">SouthEast University</a>
+		  </li>
+		</ul>
+    </div>
+	  &copy; 2021 <a href="#">UMS by ISDT Team</a>. All Rights Reserved.
+  </footer>
