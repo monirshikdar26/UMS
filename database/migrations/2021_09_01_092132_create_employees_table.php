204386d03768a431ce132209df0285ec2914e13a
monirshikdar26 <monirshikdar26@gmail.com>
2021-09-02 23:53:53 +0600

    migration table update

---
+++ database/migrations/2021_09_01_092132_create_employees_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('address')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('image')->nullable();
+            $table->string('fname')->nullable();
+            $table->string('mname')->nullable();
+            $table->string('religion')->nullable();
+            $table->date('dob')->nullable();
+            $table->date('join_date')->nullable();
+            $table->integer('designation_id')->nullable();
+            $table->double('salary')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
--- resources/views/backend/Faculty/view_faculty.blade.php
+++ resources/views/backend/Faculty/view_faculty.blade.php
@@ -11,6 +11,71 @@
       <!-- Main content -->
       <section class="content">
         <div class="row">
+
+          <div class="col-12">
+
+			 <div class="box">
+				<div class="box-header with-border">
+				    <h3 class="box-title">Faculty List</h3>
+	                <a href="{{ route('faculty.add') }}" style="float: right;" class="mb-5 btn btn-rounded btn-success"> Add Faculty</a>
+
+				</div>
+				<!-- /.box-header -->
+				<div class="box-body">
+					<div class="table-responsive">
+					  <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                        <tr>
+                            <th width="5%">SL</th>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Designation</th>
+                            <th>Mobile</th>
+                            <th>Gender</th>
+                            <th>Join Date</th>
+                            <th>Salary</th>
+
+                            <th width="25%">Action</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($allData as $key => $employee )
+                        <tr>
+                            <td>{{ $key+1 }}</td>
+                            <td> {{ $faculty->name }}</td>
+                            <td> {{ $employee->id_no }}</td>
+                            <td> {{ $employee->mobile }}</td>
+                            <td> {{ $employee->gender }}</td>
+                            <td> {{ $employee->join_date }}</td>
+                            <td> {{ $employee->salary }}</td>
+                            @if(Auth::user()->role == "Admin")
+                            <td> {{ $employee->code }}</td>
+                            @endif
+                            <td>
+                            <a href="{{ route('employee.edit',$employee->id) }}" class="btn btn-info">Edit</a>
+                            <a target="_blank" href="{{ route('employee.registration.details',$employee->id) }}" class="btn btn-danger">Details</a>
+
+                            </td>
+
+                        </tr>
+			            @endforeach
+
+						</tbody>
+						<tfoot>
+
+						</tfoot>
+					  </table>
+					</div>
+				</div>
+				<!-- /.box-body -->
+			  </div>
+			  <!-- /.box -->
+
+
+			</div>
+			<!-- /.col -->
+
         </div>
       </section>
     
